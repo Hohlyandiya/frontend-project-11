81688979143cb0a7c7d58548bd5c0e7c832d7a1b
Hohlyandiya <teroryk@gmail.com>
2025-07-30 23:51:41 +0300

    remove import

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 //import form from './components/UI/form/form'
 import registrationForm from './pageRSS/modules/registrationForm/controllers/form'
-import * as bootstrap from 'bootstrap'
 
 const app = () => {
   registrationForm()
